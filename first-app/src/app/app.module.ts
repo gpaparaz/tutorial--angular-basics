21db692cc0028b63176f1fa45a2f681421bc8720
gpaparaz <giorgia.paparazzo@gmail.com>
2023-08-22 13:35:59 +0200

    fix child

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        HomeComponent
+    ]
 })
 export class AppModule { }
